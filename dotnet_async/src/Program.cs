a5848a2aeb2b7f69b2cb9a1ead9da4c973eeae75
abhibhatt48 <47849440+abhibhatt48@users.noreply.github.com>
2022-03-17 17:33:16 +0530

    done

---
+++ dotnet_async/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+public class Program
+{
+            async static Task<string[]> read(string filename)
+            {
+                var data = await File.ReadAllLinesAsync(filename);
+                return data;
+            }
+            static async Task write(string[] data,string filename)
+            {
+                await File.WriteAllLinesAsync(filename,data);
+                System.Console.WriteLine("Done");
+                Console.WriteLine(data);
+            }
+            static public async Task perform()
+            {
+                var data = read("A.txt");
+                await write(data.Result,"B.txt");
+            }
+            public static void Main(string[] args)
+            {
+                perform();
+            }
+}
